6784370669ac5408ac57069b4be366736d29a907
Siobhan Falvey <siobhan.falvey@Nology.local>
2019-03-21 12:08:15 +0000

    ts-stuff-added

--- index.ts
+++ index.ts
@@ -1,3 +1,41 @@
-const  sayHello = () => {
-    alert('Hello!');
+class BlogPost {
+    blogTitle: string;
+    blogText: string;
+
+    constructor(title: string, text: string) {
+        this.blogTitle = title;
+        this.blogText = text;
+
+    }
+
+};
+
+let blogPosts: BlogPost[] = [];
+
+const createBlogPost = () => {
+    let form = document.getElementById("blog-form");
+    let title = form["blog-title"].value;
+    let textarea = form["blog-text"].value;
+    blogPosts.push(new BlogPost(title, textarea));
+    outputBlog(title);
+    outputBlogBody(textarea);
 }
+
+const outputBlog = (title) => {
+    let target = document.getElementById('blog-output');
+    let node = document.createElement("p");
+    let textnodetitle = document.createTextNode(title);
+    node.appendChild(textnodetitle);
+    target.appendChild(node);
+
+}
+
+const outputBlogBody = (textarea) => {
+    let target = document.getElementById('blog-output');
+    let node = document.createElement("blogbody");
+    let textnodepost = document.createTextNode(textarea);
+    node.appendChild(textnodepost);
+    target.appendChild(node);
+
+}
+
