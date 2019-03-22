2c40205e1fe98f2d11ad8b82c1186a885ded1a08
Siobhan Falvey <siobhan.falvey@Nology.local>
2019-03-22 09:20:13 +0000

    update

--- index.ts
+++ index.ts
@@ -1,49 +1,41 @@
-
 class BlogPost {
-    blogTitle: string;
-    blogText: string;
+  blogTitle: string;
+  blogText: string;
 
-    constructor(title: string, text: string) {
-        this.blogTitle = title;
-        this.blogText = text;
-
-    }
-
-};
+  constructor(title: string, text: string) {
+    this.blogTitle = title;
+    this.blogText = text;
+  }
+}
 
 let blogPosts: BlogPost[] = [];
 //let archive = [];
 
-
-
 const createBlogPost = () => {
-    let form = document.getElementById("blog-form");
-    let title = form["blog-title"].value;
-    let textarea = form["blog-text"].value;
-    blogPosts.push(new BlogPost(title, textarea));
-    outputBlog(title);
-    outputBlogBody(textarea);
-    //moveToArchive(blogPosts);
-}
+  let form = document.getElementById("blog-form");
+  let title = form["blog-title"].value;
+  let textarea = form["blog-text"].value;
+  blogPosts.push(new BlogPost(title, textarea));
+  outputBlog(title);
+  outputBlogBody(textarea);
+  //moveToArchive(blogPosts);
+};
 
-const outputBlog = (title) => {
-    let target = document.getElementById('blog-output');
-    let node = document.createElement("h2");
-    let textnodetitle = document.createTextNode(title);
-    node.appendChild(textnodetitle);
-    target.appendChild(node);
+const outputBlog = title => {
+  let target = document.getElementById("blog-output");
+  let node = document.createElement("h2");
+  let textnodetitle = document.createTextNode(title);
+  node.appendChild(textnodetitle);
+  target.appendChild(node);
+};
 
-}
-
-const outputBlogBody = (textarea) => {
-    let target = document.getElementById('blog-output');
-    let node = document.createElement("p");
-    let textnodepost = document.createTextNode(textarea);
-    node.appendChild(textnodepost);
-    target.appendChild(node);
-
-}
-
+const outputBlogBody = textarea => {
+  let target = document.getElementById("blog-output");
+  let node = document.createElement("p");
+  let textnodepost = document.createTextNode(textarea);
+  node.appendChild(textnodepost);
+  target.appendChild(node);
+};
 
 /*/const moveToArchive = (blogPosts) => {
     let oldPost = BlogPost[0];
@@ -55,14 +47,10 @@ const outputBlogBody = (textarea) => {
 // }
 
 const hideForm = () => {
-    document.getElementById('form-display').style.display = 'none';
-    document.getElementById('loading-blog').style.display = 'block';
-}
+  document.getElementById("form-display").style.display = "none";
+  document.getElementById("loading-blog").style.display = "block";
+};
 
 const showForm = () => {
-    document.getElementById('form-display').style.display = 'block';
-
-}
-
-
-
+  document.getElementById("form-display").style.display = "block";
+};
